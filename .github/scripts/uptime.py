bbe74c7d82ad7fb78d2458f5fd23e7fd1c443f03
Guionardo [GuioNote Debian] <guionardo@gmail.com>
2023-09-12 22:05:11 -0300

    Change branch for uptime

--- .github/scripts/uptime.py
+++ .github/scripts/uptime.py
@@ -1,5 +1,6 @@
 import collections
 import datetime
+import http.client
 import json
 import os
 import sys
@@ -8,10 +9,52 @@ from zoneinfo import ZoneInfo
 import httpx
 
 
+def try_paxtu_login() -> int:
+    if payload := os.getenv('PAXTU_LOGIN_PAYLOAD'):
+        print('Verificando login paxtu')
+    else:
+        print('Ignorando login paxtu (PAXTU_LOGIN_PAYLOAD inexistente)')
+        return 0
+
+    conn = http.client.HTTPSConnection("paxtu.escoteiros.org.br")
+
+    headers = {
+        'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+        'Accept': "*/*",
+        'Accept-Language': "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3",
+        'Accept-Encoding': "gzip, deflate, br",
+        'X-Requested-With': "XMLHttpRequest",
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Origin': "https://paxtu.escoteiros.org.br",
+        'DNT': "1",
+        'Alt-Used': "paxtu.escoteiros.org.br",
+        'Connection': "keep-alive",
+        'Referer': "https://paxtu.escoteiros.org.br/meupaxtu/",
+        'Cookie': "JSESSIONID=E7EFEE3A1D2EB08F2A492D874DFFB8A8; _ga_2CK8L0JG1X=GS1.1.1691107443.5.1.1691108008.0.0.0; _ga=GA1.3.740541310.1687381654; _ga_79Y7FBF0P0=GS1.1.1691107443.5.1.1691108008.0.0.0",
+        'Sec-Fetch-Dest': "empty",
+        'Sec-Fetch-Mode': "cors",
+        'Sec-Fetch-Site': "same-origin",
+        'TE': "trailers"
+    }
+
+    try:
+        conn.request("POST", "/meupaxtu/loginMeuSigue.do", payload, headers)
+
+        res = conn.getresponse()
+
+        data = res.read()
+
+        print(data.decode("utf-8"))
+        return res.status
+    except Exception as exc:
+        print(exc)
+        return 500
+
+
 def get_now():
-    dt = datetime.datetime.now(datetime.timezone.utc)
-    utc_time = dt.replace(tzinfo=datetime.timezone.utc)
-    return utc_time
+    return (datetime.datetime
+            .utcnow()
+            .replace(tzinfo=datetime.timezone.utc))
 
 
 def get_timestamp():
@@ -32,9 +75,11 @@ URLS = ['https://github.com', 'https://paxtu.escoteiros.org.br',
         'https://paxtu.escoteiros.org.br/meupaxtu']
 
 data_file = os.path.abspath(
-    'uptime.json' if not len(sys.argv) > 1 else sys.argv[1])
+    'uptime.json' if len(sys.argv) <= 1 else sys.argv[1]
+)
 uptime_md_file = os.path.abspath(
-    'uptime.md' if not len(sys.argv) > 2 else sys.argv[2])
+    'uptime.md' if len(sys.argv) <= 2 else sys.argv[2]
+)
 
 print('Reading data file:', data_file)
 try:
@@ -108,13 +153,20 @@ def get_data_statuses(data):
     return f'{last_days_statuses}|{today_statuses}'
 
 
+PAXTU_LOGIN = 'Paxtu Login'
+
 for url in URLS:
     result = (get_timestamp(), check_url(url))
     if url not in uptime_data:
         uptime_data[url] = []
 
     uptime_data[url].append(result)
+
     print(f'URL {url} -> {result[1]} ({len(uptime_data[url])})')
+
+if PAXTU_LOGIN not in uptime_data:
+    uptime_data[PAXTU_LOGIN] = []
+uptime_data[PAXTU_LOGIN].append((get_timestamp(), try_paxtu_login()))
 
 try:
     with open(data_file, 'w') as file:
@@ -128,6 +180,6 @@ with open(uptime_md_file, 'w') as file:
     file.write(
         f'# Monitoramento\n\nÚltima verificação: {get_localtime(get_now())}\n\n')
     file.write('|Serviço|Status|Últimas 24h|\n|---|---|---|\n')
-    for url in URLS:
+    for url in URLS+[PAXTU_LOGIN]:
         if data := uptime_data.get(url):
             file.write(f'|{url}|{get_data_statuses(data)}|\n')
