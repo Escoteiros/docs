b51e9d0de96ce0032c23df002c17574732b7e0c9
Guionardo [GuioNote Debian] <guionardo@gmail.com>
2023-09-12 23:19:57 -0300

    WIP

--- .github/scripts/uptime.py
+++ .github/scripts/uptime.py
@@ -4,9 +4,45 @@ import http.client
 import json
 import os
 import sys
-from zoneinfo import ZoneInfo
+import zoneinfo
 
 import httpx
+
+ziBR = zoneinfo.ZoneInfo('America/Sao_Paulo')
+
+
+class DataHora():
+
+    def __init__(self, obj=None):
+        self._dh = (datetime.datetime
+                    .now()
+                    .astimezone(datetime.timezone.utc))
+        if isinstance(obj, int):
+            self._dh = datetime.datetime.fromtimestamp(
+                obj, tz=datetime.timezone.utc)
+        elif isinstance(obj, datetime.datetime):
+            if obj.tzinfo:
+                if obj.tzinfo == datetime.timezone.utc:
+                    self._dh = obj
+                else:
+                    self._dh = obj.astimezone(datetime.timezone.utc)
+        self._dh = self._dh.replace(second=0, microsecond=0)
+
+    def __str__(self) -> str:
+        return self._dh.astimezone(ziBR).strftime('%d/%m/%Y %H:%M:%S %Z')
+
+    def __lt__(self, other):
+        return self._dh < other._dh
+
+    def sub(self, interval: datetime.timedelta):
+        self._dh -= interval
+        return self
+
+    def date(self) -> datetime.date:
+        return self._dh.date()
+
+    def timestamp(self) -> int:
+        return int(self._dh.timestamp())
 
 
 def try_paxtu_login() -> int:
@@ -42,32 +78,12 @@ def try_paxtu_login() -> int:
 
         res = conn.getresponse()
 
-        data = res.read()
+        _ = res.read()
 
-        print(data.decode("utf-8"))
         return res.status
     except Exception as exc:
         print(exc)
         return 500
-
-
-def get_now():
-    return (datetime.datetime
-            .utcnow()
-            .replace(tzinfo=datetime.timezone.utc))
-
-
-def get_timestamp():
-    return int(get_now().timestamp())
-
-
-def from_timestamp(timestamp: int) -> datetime.datetime:
-    dt = datetime.datetime.fromtimestamp(timestamp, datetime.timezone.utc)
-    return dt.replace(second=0, microsecond=0)
-
-
-def get_localtime(ts: datetime.datetime) -> datetime.datetime:
-    return ts.replace(tzinfo=ZoneInfo('America/Sao_Paulo'))
 
 
 URLS = ['https://github.com', 'https://paxtu.escoteiros.org.br',
@@ -106,12 +122,12 @@ def check_url(url: str) -> int:
 
 
 def get_data_statuses(data):
-    last_24h = (get_now()-datetime.timedelta(days=1)
-                ).replace(second=0, microsecond=0)
+    last_24h = DataHora().sub(datetime.timedelta(days=1))
+
     old_statuses = collections.defaultdict(list)
     statuses = {}
     for (timestamp, status_code) in data:
-        dt = from_timestamp(timestamp)
+        dt = DataHora(timestamp)
         if dt < last_24h:
             old_statuses[dt.date()].append(status_code)
         else:
@@ -148,7 +164,7 @@ def get_data_statuses(data):
     today_statuses = ''
     for t in today_times:
         icon = '✔️' if 0 < statuses[t] < 400 else '❌'
-        today_statuses += f'<span title="{get_localtime(t)} : {statuses[t]}">{icon}</span>'
+        today_statuses += f'<span title="{t} : {statuses[t]}">{icon}</span>'
 
     return f'{last_days_statuses}|{today_statuses}'
 
@@ -156,7 +172,7 @@ def get_data_statuses(data):
 PAXTU_LOGIN = 'Paxtu Login'
 
 for url in URLS:
-    result = (get_timestamp(), check_url(url))
+    result = (DataHora().timestamp(), check_url(url))
     if url not in uptime_data:
         uptime_data[url] = []
 
@@ -166,7 +182,10 @@ for url in URLS:
 
 if PAXTU_LOGIN not in uptime_data:
     uptime_data[PAXTU_LOGIN] = []
-uptime_data[PAXTU_LOGIN].append((get_timestamp(), try_paxtu_login()))
+pl_ts = DataHora().timestamp()
+pl_res = try_paxtu_login()
+uptime_data[PAXTU_LOGIN].append((pl_ts, pl_res))
+print(f'PAXTU LOGIN -> {pl_res} ({len([PAXTU_LOGIN])})')
 
 try:
     with open(data_file, 'w') as file:
@@ -178,7 +197,7 @@ except Exception as exc:
 print('Saving markdown ', uptime_md_file)
 with open(uptime_md_file, 'w') as file:
     file.write(
-        f'# Monitoramento\n\nÚltima verificação: {get_localtime(get_now())}\n\n')
+        f'# Monitoramento\n\nÚltima verificação: {DataHora()}\n\n')
     file.write('|Serviço|Status|Últimas 24h|\n|---|---|---|\n')
     for url in URLS+[PAXTU_LOGIN]:
         if data := uptime_data.get(url):
